7b116dba881a7238ffa660ca84691bea883eff88
narinthon sonchaiyaphum <54059270+narinthon-so@users.noreply.github.com>
2023-09-22 16:16:31 +0700

    Update try select from DB but not finish yet

--- sqlite/index.ts
+++ sqlite/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+// console.log("Hello via Bun!");
+/*
+SELECT 
+    m.*, d.name, d.country
+FROM matchs m 
+JOIN divisions d ON d.division == m.Div
+where m.Div = "E0"
+limit 10;
+*/
+const SQL1 = "SELECT m.*, d.name, d.country FROM matchs m JOIN divisions d ON d.division == m.Div where m.Div = 'E0' limit 10";
+
+import { Database } from "bun:sqlite";
+
+const db = new Database("european_database.sqlite");
